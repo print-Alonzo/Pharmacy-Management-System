2775ed46e7148a20766414eacf3ce718815515be
Joel Ethan Batac <bjoelethan@gmail.com>
2023-11-21 20:53:41 +0800

    ✅Finished Frontend for Report #3

--- Pharmacy Inventory Management System/src/java/pharmacysystem/stockreport.java
+++ Pharmacy Inventory Management System/src/java/pharmacysystem/stockreport.java
@@ -29,12 +29,32 @@ public class stockreport {
     public int generateStockReport(){
         try{
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/pharmacy_db?allowPublicKeyRetrieval=true&useTimezone=true&serverTimezone=UTC&user=root&password=12345678&useSSL=false");
-            String query1 = "SELECT i.medicine_id, i.brand_name, YEAR(s.dateReceived) AS 'year_report', MONTH(s.dateReceived) AS 'month_report', COUNT(s.stock_id) AS 'Amount Received' \n" +
-                            "FROM medicine_stock s LEFT JOIN medicine_info i \n" + 
-                                                "ON i.medicine_id = s.medicine_id \n" + 
-                            "GROUP BY i.medicine_id, year_report, month_report \n" +
-                            "ORDER BY year_report, month_report" ;
-            PreparedStatement ps1 = conn.prepareStatement(query1);
+            String query1 = "SELECT i.medicine_id, i.brand_name, YEAR(s.dateReceived) AS 'year_report', MONTH(s.dateReceived) AS 'month_report', COUNT(s.stock_id) AS 'Amount_Received'\n" +
+"                            FROM medicine_stock s LEFT JOIN medicine_info i\n" +
+"                                                ON i.medicine_id = s.medicine_id\n" +
+"                            GROUP BY i.medicine_id, year_report, month_report\n" +
+"                            ORDER BY year_report, month_report, Amount_Received;\n";
+            
+            medicineIDList1.clear();
+            brandNameList1.clear();
+            yearList1.clear();
+            monthList1.clear();
+            received1.clear();
+            
+            medicineIDList2.clear();
+            brandNameList2.clear();
+            yearList2.clear();
+            monthList2.clear();
+            expired2.clear();
+            
+            medicineIDList3.clear();
+            brandNameList3.clear();
+            yearList3.clear();
+            monthList3.clear();
+            sold3.clear();
+            
+            PreparedStatement ps1 = conn.prepareStatement(query1);            
+            
             ResultSet rs1 = ps1.executeQuery();
             while(rs1.next()){
                 // Extract data from the result set
@@ -42,7 +62,7 @@ public class stockreport {
                 String brandName = rs1.getString("brand_name");
                 int yearReport = rs1.getInt("year_report");
                 int monthReport = rs1.getInt("month_report");
-                int amountReceived = rs1.getInt("Amount Received");
+                int amountReceived = rs1.getInt("Amount_Received");
                 
                 // Populate the ArrayLists
                 medicineIDList1.add(medicineID);
@@ -60,7 +80,7 @@ public class stockreport {
                             "                      LEFT JOIN medicine_info i\n" +
                             "                      ON s.medicine_id = i.medicine_id\n" +
                             "GROUP BY i.medicine_id, year_report, month_report\n" +
-                            "ORDER BY year_report, month_report";
+                            "ORDER BY year_report, month_report, 'Amount Sold'";
             PreparedStatement ps2 = conn.prepareStatement(query2);
             ResultSet rs2 = ps2.executeQuery();
             while(rs2.next()){
@@ -80,12 +100,12 @@ public class stockreport {
             }
             ps2.close();
             
-            String query3 = "SELECT s.medicine_id, i.brand_name, YEAR(s.dateExpire) AS 'year_report', MONTH(s.dateExpire) AS 'month_report', COUNT(s.stock_id) AS 'Amount Expiring'\n" +
+            String query3 = "SELECT s.medicine_id, i.brand_name, YEAR(s.dateExpire) AS 'year_report', MONTH(s.dateExpire) AS 'month_report', COUNT(s.stock_id) AS 'Amount_Expiring'\n" +
                             "FROM medicine_stock s LEFT JOIN medicine_info i\n" +
                             "					  ON s.medicine_id = i.medicine_id\n" +
                             "WHERE s.transactionID IS NULL\n" +
                             "GROUP BY s.medicine_id, year_report, month_report\n" +
-                            "ORDER BY year_report, month_report";
+                            "ORDER BY year_report, month_report, 'Amount_Expiring'";
             PreparedStatement ps3 = conn.prepareStatement(query3);
             ResultSet rs3 = ps3.executeQuery();
             while(rs3.next()){
@@ -94,7 +114,7 @@ public class stockreport {
                 String brandName = rs3.getString("brand_name");
                 int yearReport = rs3.getInt("year_report");
                 int monthReport = rs3.getInt("month_report");
-                int amountReceived = rs3.getInt("Amount Expiring");
+                int amountReceived = rs3.getInt("Amount_Expiring");
                 
                 // Populate the ArrayLists
                 medicineIDList2.add(medicineID);
@@ -119,7 +139,7 @@ public class stockreport {
     public static void main(String[] args){
         stockreport sr = new stockreport();
         sr.generateStockReport();
-        for(String brand : sr.brandNameList2){
+        for(String brand : sr.brandNameList1){
             System.out.println(brand);
         }
     }
---
+++ Pharmacy Inventory Management System/web/monthly_stock_report.jsp
@@ -0,0 +1,139 @@
+<%-- 
+    Document   : monthly_stock_report
+    Created on : 11 21, 23, 5:12:01 PM
+    Author     : ccslearner
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@page import="java.util.*, pharmacysystem.*" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Monthly Stock Report</title>
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            background-color: #f8f9fa;
+            margin: 0;
+            padding: 20px;
+            text-align: center;
+        }
+
+        h1 {
+            color: #007bff;
+        }
+
+        h2 {
+            color: #007bff;
+            margin-top: 30px;
+        }
+
+        table {
+            width: 80%;
+            margin: 20px auto;
+            border-collapse: collapse;
+            text-align: left;
+        }
+
+        th, td {
+            padding: 10px;
+            border-bottom: 1px solid #dee2e6;
+        }
+
+        th {
+            background-color: #007bff;
+            color: #fff;
+        }
+
+        tbody tr:hover {
+            background-color: #f5f5f5;
+        }
+    </style>
+</head>
+<body>
+    <h1>Monthly Stock Report</h1>
+    <jsp:useBean id="A" class="pharmacysystem.stockreport" scope="session" />
+    <%
+        A.generateStockReport();
+    %>
+
+    <div>
+        <h2>Stocks Received:</h2>
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>Year</th>
+                    <th>Month</th>
+                    <th>Medicine ID</th>
+                    <th>Brand Name</th>
+                    <th>Amount Received</th>
+                </tr>
+            </thead>
+            <tbody>
+                <% for(int i = 0; i < A.medicineIDList1.size(); i++){ %>
+                    <tr>
+                        <td><%= A.yearList1.get(i)%></td>
+                        <td><%= A.monthList1.get(i)%></td>
+                        <td><%= A.medicineIDList1.get(i)%></td>
+                        <td><%= A.brandNameList1.get(i)%></td>
+                        <td><%= A.received1.get(i)%></td>
+                    </tr>
+                <% System.out.println(i);} %>
+            </tbody>
+        </table>
+    </div>
+
+    <div>
+        <h2>Stocks Sold:</h2>
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>Year</th>
+                    <th>Month</th>
+                    <th>Medicine ID</th>
+                    <th>Brand Name</th>
+                    <th>Stocks Sold</th>
+                </tr>
+            </thead>
+            <tbody>
+                <% for(int i = 0; i < A.medicineIDList3.size(); i++){ %>
+                    <tr>
+                        <td><%= A.yearList3.get(i)%></td>
+                        <td><%= A.monthList3.get(i)%></td>
+                        <td><%= A.medicineIDList3.get(i)%></td>
+                        <td><%= A.brandNameList3.get(i)%></td>
+                        <td><%= A.sold3.get(i)%></td>
+                    </tr>
+                <% System.out.println(i);} %>
+            </tbody>
+        </table>
+    </div>
+    
+    <div>
+        <h2>Stock Expiry Dates:</h2>
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>Year</th>
+                    <th>Month</th>
+                    <th>Medicine ID</th>
+                    <th>Brand Name</th>
+                    <th>Stocks to Expire</th>
+                </tr>
+            </thead>
+            <tbody>
+                <% for(int i = 0; i < A.medicineIDList2.size(); i++){ %>
+                    <tr>
+                        <td><%= A.yearList2.get(i)%></td>
+                        <td><%= A.monthList2.get(i)%></td>
+                        <td><%= A.medicineIDList2.get(i)%></td>
+                        <td><%= A.brandNameList2.get(i)%></td>
+                        <td><%= A.expired2.get(i)%></td>
+                    </tr>
+                <% System.out.println(i);} %>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
